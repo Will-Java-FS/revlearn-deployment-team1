d4ae3714955a131cb43716045cd09dbbb4b6568b
Trey Crossley <treycrossley@gmail.com>
2024-09-09 15:27:24 -0700

    trying something out

---
+++ terraform/rds/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+
+  required_version = ">= 1.8.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "revlearn-tfstate"  # Replace with the name of your S3 bucket
+    region         = "us-east-1"                    # Ensure this matches your bucket's region
+    dynamodb_table = "app-state"              # Replace with the name of your DynamoDB table
+    encrypt        = true
+    key            = "terraform.tfstate"
+  }
+}
