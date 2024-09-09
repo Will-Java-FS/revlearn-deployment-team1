73884bbf5329e1451798081a2dc4809011e7922b
Trey Crossley <treycrossley@gmail.com>
2024-09-09 11:13:20 -0700

    kafka ec2 terraform

---
+++ terraform/modules/kafka/security_groups.tf
@@ -0,0 +1,37 @@
+
+
+
+
+# Define the security group for Kafka
+resource "aws_security_group" "kafka_sg" {
+  name        = "kafka_sg"
+  description = "Allow Kafka and SSH access"
+
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9092
+    to_port     = 9092
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name  = "kafka-sg-tc"
+    Owner = "Trey-Crossley"
+  }
+}
