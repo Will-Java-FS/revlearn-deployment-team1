3ba2928198aea69a7f6c80ca895e2b66e1035494
Trey Crossley <treycrossley@gmail.com>
2024-09-10 14:49:12 -0700

    change security group for db

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "rds_sg" {
   vpc_id = var.vpc_id
 
   ingress {
-    from_port   = 3306
-    to_port     = 3306
+    from_port   = 5432
+    to_port     = 5432
     protocol    = "tcp"
     cidr_blocks = var.cidr_blocks
   }
