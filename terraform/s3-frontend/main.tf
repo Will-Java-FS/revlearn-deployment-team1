0c10d3c58648b8820f1f728a4fa9b69fd345d31d
Trey Crossley <treycrossley@gmail.com>
2024-09-10 09:42:00 -0700

    well main means nothing now doesnt it

--- terraform/s3-frontend/main.tf
+++ terraform/s3-frontend/main.tf
@@ -8,6 +8,32 @@ resource "aws_s3_bucket" "frontend_build" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "s3_ownership" {
+  bucket = aws_s3_bucket.frontend_build.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_public_access" {
+  bucket = aws_s3_bucket.frontend_build.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "s3_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.s3_ownership,
+    aws_s3_bucket_public_access_block.s3_public_access,
+  ]
+
+  bucket = aws_s3_bucket.frontend_build.id
+  acl    = "public-read"
+}
+
 resource "aws_s3_bucket_website_configuration" "frontend_build_website" {
   bucket = aws_s3_bucket.frontend_build.id
 
